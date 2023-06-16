99c3b1c1512220d931aed73352282f25b14920cf
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-16 18:08:14 +0300

    Update application.dart

--- lib/iternal/application.dart
+++ lib/iternal/application.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Butt Committee',
+      title: 'ApplicationName',
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
         useMaterial3: true,
