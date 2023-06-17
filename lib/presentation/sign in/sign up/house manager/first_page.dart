71456e1bb643890a4f4e204b1353d63decf8cca1
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-17 20:39:13 +0300

    Update first_page.dart

--- lib/presentation/sign in/sign up/house manager/first_page.dart
+++ lib/presentation/sign in/sign up/house manager/first_page.dart
@@ -1,46 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentation/sign%20in/widgets/entry_filed.dart';
 import 'package:flutter_application_1/presentation/sign%20in/sign%20up/house%20manager/change_house.dart';
+import 'package:flutter_application_1/presentation/sign%20in/widgets/main_name_page.dart';
 
-class SignUpHouseManager extends StatelessWidget {
-  final EntryField city = EntryField(label: 'City');
-  final EntryField street = EntryField(label: 'Street');
-  final EntryField house = EntryField(label: 'House');
+class SignUpHouseManager extends StatefulWidget {
+  final List<TextEditingController> listController = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController()
+  ];
+  final List<String> listLabel = ['City', 'Street', 'House'];
+  SignUpHouseManager({super.key});
 
+  @override
+  State<SignUpHouseManager> createState() => _SignUpHouseManagerState();
+}
+
+class _SignUpHouseManagerState extends State<SignUpHouseManager> {
+  List<bool> listValidate = [false, false, false];
   //ЗАГЛУШКА
   void _onClick() {
-    print(
-        city.resualtText + " " + street.resualtText + " " + house.resualtText);
+    String result = '';
+    for (int i = 0; i < widget.listController.length; i++) {
+      result += "${widget.listController[i].text} ";
+    }
+    print(result);
   }
-  //ЗАГЛУШКА
 
-  SignUpHouseManager({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: [
-          const Padding(
-              padding: EdgeInsets.only(top: 60, left: 30, bottom: 10),
-              child: Text('Location of the building',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500))),
-          city,
-          street,
-          house,
-          Padding(
-              padding: const EdgeInsets.all(20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            const MainNamePage(text: 'Location of the building'),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: widget.listController.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: widget.listLabel[index],
+                      errorText: listValidate[index]
+                          ? '${widget.listLabel[index]} is Empty'
+                          : null,
+                    ),
+                    controller: widget.listController[index],
+                    onChanged: (value) {
+                      setState(() {
+                        listValidate[index] =
+                            widget.listController[index].text == '';
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
               child: ElevatedButton(
-                  onPressed: () {
+                onPressed: () {
+                  bool isCompletedForm = true;
+                  setState(() {
+                    for (int i = 0; i < widget.listController.length; i++) {
+                      if (widget.listController[i].text == '') {
+                        isCompletedForm = false;
+                        listValidate[i] = true;
+                      }
+                    }
+                  });
+
+                  if (isCompletedForm == true) {
                     _onClick(); //ЗАГЛУШКА
                     Navigator.of(context).push(MaterialPageRoute(
                         builder: (context) => const ChangeHouse()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white),
-                  child: const Text('Go to design'))),
-        ],
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white),
+                child: const Text('Go to design'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
