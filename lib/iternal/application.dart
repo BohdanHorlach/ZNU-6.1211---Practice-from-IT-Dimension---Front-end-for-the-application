1a6204d440cc4562caa4391063d3af0fcd7e712b
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-19 22:47:10 +0300

    Add files via upload

--- lib/iternal/application.dart
+++ lib/iternal/application.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentation/sign in/sign up/house manager/first_page.dart';
+import '../presentation/sign in/sign up/house manager/sign_up_house_manager.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,12 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ApplicationName',
+      title: 'Application',
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
         useMaterial3: true,
       ),
-      home: SignUpHouseManager(),
+      home: const SignUpHouseManager(),
     );
   }
 }
