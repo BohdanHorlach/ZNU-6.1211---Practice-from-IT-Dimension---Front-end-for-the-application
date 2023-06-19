1188a1fa5419bdbf4662e00b11671f8f91228b53
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-19 22:44:14 +0300

    Add files via upload

---
+++ lib/presentation/sign in/sign up/house manager/adding_personal_info.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import '../house%20manager/choice_of_voting_type.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/main_name_page.dart';
+
+class AddingPersonalInfo extends StatefulWidget {
+  const AddingPersonalInfo({super.key});
+
+  @override
+  State<AddingPersonalInfo> createState() => _AddingPersonalInfoState();
+}
+
+class _AddingPersonalInfoState extends State<AddingPersonalInfo> {
+  final listController = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  final listLabel = [
+    'First name Last Name',
+    'E-mail',
+    'Phone Number',
+    'Password'
+  ];
+
+  final listTypeInput = [
+    TextInputType.text,
+    TextInputType.emailAddress,
+    TextInputType.number,
+    TextInputType.visiblePassword
+  ];
+
+  List<bool> validateList = List.filled(4, false);
+
+  bool isCorrectEntryinfo() {
+    bool result = true;
+    for (int i = 0; i < listController.length; i++) {
+      if (listController[i].text == '') {
+        setState(() {
+          validateList[i] = true;
+        });
+        result = false;
+      }
+    }
+    if (listController[1].text.contains('@gmail.com') == false) {
+      setState(() {
+        validateList[1] = true;
+      });
+      result = false;
+    }
+
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            const MainNamePage(text: 'Personal Information'),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: listController.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: TextField(
+                    controller: listController[index],
+                    keyboardType: listTypeInput[index],
+                    obscureText: index == 3 ? true : false,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: listLabel[index],
+                      errorText: validateList[index]
+                          ? '${listLabel[index]} is incorrect'
+                          : null,
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        validateList[index] = listController[index].text == '';
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: CustomButton(
+                textButton: 'Go Next',
+                onClick: () {
+                  bool isCompletedForm = isCorrectEntryinfo();
+                  if (isCompletedForm == true) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ChoiceVotinType(
+                            firstAndLastName: listController[0].text)));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/sign in/sign up/house manager/choice_of_voting_type.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/sign%20in/widgets/main_name_page.dart';
+import '../../widgets/check_status/verifying_for_document.dart';
+import '../../widgets/check_status/voting.dart';
+import '../../widgets/check_status/first_resident.dart';
+import '../../widgets/custom_button.dart';
+
+class ChoiceVotinType extends StatefulWidget {
+  final String firstAndLastName;
+  const ChoiceVotinType({super.key, required this.firstAndLastName});
+
+  @override
+  State<ChoiceVotinType> createState() => _ChoiceVotinTypeState();
+}
+
+class _ChoiceVotinTypeState extends State<ChoiceVotinType> {
+  VerifyingDocument verifyingDocument = VerifyingDocument();
+  Voting voting = Voting();
+  FirstResident firstResident = FirstResident();
+  List<Widget> listShowWidgets = [];
+  List<String> listChoice = [
+    'I have a document',
+    'I was elected by vote',
+    'I am the first tenant to install the app'
+  ];
+  String dropdownValue = '';
+
+  int indexShowWidget = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    dropdownValue = listChoice[0];
+    listShowWidgets = [verifyingDocument, voting, firstResident];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              child: const MainNamePage(text: 'Status check'),
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+            InputDecorator(
+              decoration: InputDecoration(
+                labelText: widget.firstAndLastName,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.keyboard_arrow_down),
+                iconSize: 25,
+                elevation: 12,
+                isExpanded: true,
+                isDense: true,
+                focusColor: Colors.white,
+                underline: Container(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                    for (int i = 0; i < listChoice.length; i++) {
+                      if (dropdownValue == listChoice[i]) {
+                        indexShowWidget = i;
+                        break;
+                      }
+                    }
+                  });
+                },
+                items: listChoice.map<DropdownMenuItem<String>>((choice) {
+                  return DropdownMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList(),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20), child: Divider()),
+            listShowWidgets[indexShowWidget],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: CustomButton(
+                textButton: 'Save',
+                onClick: () {
+                  print(listShowWidgets[indexShowWidget].toStringShort());
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
