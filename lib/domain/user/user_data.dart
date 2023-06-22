a6c52ff55501039dd03589fa3aa5d7b2752958ff
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-22 14:13:23 +0300

    Update user_data.dart

--- lib/domain/user/user_data.dart
+++ lib/domain/user/user_data.dart
@@ -1,11 +1,46 @@
+import 'package:flutter/widgets.dart';
 import 'apartment_data.dart';
 
 enum UserType { houseManager, resident, company, serviceProvider }
 
-class UserData {
-  UserType typeOfUser = UserType.resident;
-  String email = '';
-  String phone = '';
-  bool status = false;
-  ApartmentData apartmentData = ApartmentData();
+class UserData with ChangeNotifier {
+  UserType _typeOfUser = UserType.resident;
+  String _firstAndLastName = '';
+  String _email = '';
+  String _phone = '';
+  bool _status = false;
+  ApartmentData _apartmentData = ApartmentData();
+
+  UserType get getTypeUser => _typeOfUser;
+  String get getFirstAndLastName => _firstAndLastName;
+  String get getEmail => _email;
+  String get getPhone => _phone;
+  bool get getStatus => _status;
+  ApartmentData get getApartmentData => _apartmentData;
+
+  void changeType(UserType newType) {
+    _typeOfUser = newType;
+
+    notifyListeners();
+  }
+
+  void changeBasedData(String newName, String newEmail, String newPhone) {
+    _firstAndLastName = newName;
+    _email = newEmail;
+    _phone = newPhone;
+
+    notifyListeners();
+  }
+
+  void changeStatus(bool newStatus) {
+    _status = newStatus;
+
+    notifyListeners();
+  }
+
+  void changeApartmentData(ApartmentData newData) {
+    _apartmentData = newData;
+
+    notifyListeners();
+  }
 }
