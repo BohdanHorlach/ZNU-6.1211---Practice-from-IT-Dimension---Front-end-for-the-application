5e12fc714518cf6bad6e72a0454618cbc3906386
Bohdan <99806409+BohdanHorlach@users.noreply.github.com>
2023-06-22 12:59:58 +0300

    Add files via upload

---
+++ lib/domain/user/user_data.dart
@@ -0,0 +1,10 @@
+import 'apartment_data.dart';
+
+class UserData {
+  String firstAndLastName = '';
+  String email = '';
+  String phone = '';
+  String linkToWarsUp = '';
+  bool status = false;
+  ApartmentData apartmentData = ApartmentData();
+}
